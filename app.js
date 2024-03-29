cb239f431e6e289387a5994152c7d50151015da8
Maftuna Raimova <maftuna.a.raimova@gmail.com>
2024-03-29 17:43:25 +0500

    Phoenix-corporative

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const db = require('./db'); // db connection 
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/checkouts", async (req, res) => { 
+	const sql = "SELECT * FROM checkouts";
+	
+	const result  = await db.query(sql);
+	res.status(201).json(result.rows);
+	  
+  }); // get requst to get all data
+
+  app.post("/checkout", (req, res) => {
+	console.log(new Date());
+	const { fullname, 
+		    email, 
+			phone_number, 
+			reason, 
+			message } = req.body;
+	const date = new Date().toISOString(); // post request to insert new data 
+  
+	db.query(`INSERT INTO checkouts (fullname,email,phone_number,reason,messages) VALUES ($1,$2,$3,$4,$5)`,[fullname, email, phone_number, reason, message])
+	res.status(201).json({ fullname, email, phone_number, reason, message });
+	  
+  }); // to insert values to the query
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => 
+{
+	console.log('Server is running on port ${PORT}');
+});
